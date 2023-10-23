984a7bfd3ce1d2cca9e93c8610d2ccab2b717dcf
Andrey Frolov <senior.slender2015@gmail.com>
2023-10-23 13:48:48 +0300

    Building web form, store and retrieve data with django models, views and templates

--- bwc_portal/bwc_app/urls.py
+++ bwc_portal/bwc_app/urls.py
@@ -1,11 +1,11 @@
-from django.contrib import admin
+from . import views
 from django.urls import path
-from .views import home, readers, shopping, save
 
 urlpatterns = [
-    path('', home),
-    path('home', home),
-    path('readers', readers),
-    path('shopping', shopping),
-    path('save', save),
+    path('', views.home, name='home'),
+    path('home', views.home),
+    path('readers/', views.readers_tab, name='readers_tab'),
+    path('shopping', views.shopping, name='shopping'),
+    path('save', views.save, name='save'),
+    path('readers/add', views.save_reader, name='save_reader'),
 ]
--- bwc_portal/bwc_app/views.py
+++ bwc_portal/bwc_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib import admin
 from django.http import HttpResponse
+from .models import Reader
 
 
 def home(request):
@@ -18,3 +18,23 @@ def shopping(request):
 def save(request):
     name = request.POST['name']
     return render(request, 'welcome.html', context={'name': name})
+
+
+def readers_tab(request):
+    readers = Reader.objects.all()
+    return render(request, 'readers.html', context={
+        'current_tab': 'readers',
+        'readers': readers
+    })
+
+
+def save_reader(request):
+    reader_item = Reader(
+        reference_id=request.POST['reader_ref_id'],
+        reader_name=request.POST['reader_name'],
+        reader_contact=request.POST['reader_contact'],
+        reader_address=request.POST['address'],
+        active=True
+    )
+    reader_item.save()
+    return redirect('/readers')
